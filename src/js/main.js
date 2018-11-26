a0d72f98ef6ee6a26edc15d29953708a766fee95
Bodnarchuk Valerii <bodnarchukvalole@gmail.com>
2018-11-27 00:33:46 +0200

    make js tab

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,35 @@
-let a = "hello"
-console.log(a);
+// Anchor animation
+
+$(document).ready(function(){
+//     $("a").on('click', function (event) {
+//         event.preventDefault();
+//         var id  = $(this).attr('href'),
+//             top = $(id).offset().top;
+//         $('body,html').animate({scrollTop: top}, 1500);
+//     });
+// });
+
+	// Tab
+
+	$(".program-tab__inner-status").not(":first").hide();
+
+	// Set attribute data-tab
+
+	$(".program-tab__item").each(function(i){
+		$(this).attr('data-tab', 'tab'+ i)
+	});
+	$(".program-tab__inner-status").each(function(i){
+		$(this).attr('data-tab', 'tab'+ i)
+	});
+
+	$(".program-tab__item").on('click', function(e){
+		e.preventDefault();
+		let dataTab = $(this).data('tab');
+		// Change menu
+		$(".program-tab__item").removeClass('program-tab__item_active');
+    	$(this).addClass('program-tab__item_active');
+    	// Change content of tabs
+    	$(".program-tab__inner-status").hide();
+		$(".program-tab__inner-status").filter('[data-tab='+dataTab+']').show();
+	})
+})
