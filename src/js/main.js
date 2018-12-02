9e08ecc1b0ba1a663472c9a55bfcd097c3d8ae27
Bodnarchuk Valerii <bodnarchukvalole@gmail.com>
2018-12-02 20:46:37 +0200

    Create ancher-top and js for him

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,45 @@
-// Anchor animation
+	// Anchor animation
 
-$(document).ready(function(){
-    $("a[href^='#']").on('click', function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
-    })
+$("a[href^='#']").on('click', function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+})
 
 	// Tab
 
-	$(".program-tab__inner-status").not(":first").hide();
+$(".program-tab__inner-status").not(":first").hide();
 
 	// Set attribute data-tab
 
-	$(".program-tab__item").each(function(i){
-		$(this).attr('data-tab', 'tab'+ i)
-	});
-	$(".program-tab__inner-status").each(function(i){
-		$(this).attr('data-tab', 'tab'+ i)
-	});
+$(".program-tab__item").each(function(i){
+	$(this).attr('data-tab', 'tab'+ i)
+});
+$(".program-tab__inner-status").each(function(i){
+	$(this).attr('data-tab', 'tab'+ i)
+});
 
-	$(".program-tab__item").on('click', function(e){
-		e.preventDefault();
-		let dataTab = $(this).data('tab');
-		// Change menu
-		$(".program-tab__item").removeClass('program-tab__item_active');
-    	$(this).addClass('program-tab__item_active');
-    	// Change content of tabs
-    	$(".program-tab__inner-status").hide();
-		$(".program-tab__inner-status").filter('[data-tab='+dataTab+']').show();
+$(".program-tab__item").on('click', function(e){
+	e.preventDefault();
+	let dataTab = $(this).data('tab');
+	// Change menu
+	$(".program-tab__item").removeClass('program-tab__item_active');
+	$(this).addClass('program-tab__item_active');
+	// Change content of tabs
+	$(".program-tab__inner-status").hide();
+	$(".program-tab__inner-status").filter('[data-tab='+dataTab+']').show();
+});
+
+	// Anchor-top
+
+	let headerOffset = $(".header").height();
+	$(window).scroll(function() {
+		let scrollPos = $(window).scrollTop();
+		if (scrollPos >= headerOffset) {
+			$(".ancher-top").addClass("ancher-top_active");
+		} else {
+			$(".ancher-top").removeClass("ancher-top_active");
+		}
 	})
-})
+
