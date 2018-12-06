285a32173fb69e608d3e7002236f9c22953d1f4f
Bodnarchuk Valerii <bodnarchukvalole@gmail.com>
2018-12-06 16:27:38 +0200

    Delete accesive code, make code in one style, change mixin of block program

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,15 @@
 // Anchor animation
 
 $("a[href^='#']").on('click', function (event) {
-    event.preventDefault();
-    var id  = $(this).attr('href'),
-        top = $(id).offset().top;
-    $('body,html').animate({scrollTop: top}, 1500);
+	event.preventDefault();
+	var id  = $(this).attr('href'),
+		top = $(id).offset().top;
+	$('body,html').animate({scrollTop: top}, 1500);
 })
 
 // Tab
 
 $(".program-tab__inner-status").not(":first").hide();
-
-	// Set attribute data-tab
 
 $(".program-tab__item").each(function(i){
 	$(this).attr('data-tab', 'tab'+ i)
@@ -23,10 +21,8 @@ $(".program-tab__inner-status").each(function(i){
 $(".program-tab__item").on('click', function(e){
 	e.preventDefault();
 	let dataTab = $(this).data('tab');
-	// Change menu
 	$(".program-tab__item").removeClass('program-tab__item_active');
 	$(this).addClass('program-tab__item_active');
-	// Change content of tabs
 	$(".program-tab__inner-status").hide();
 	$(".program-tab__inner-status").filter('[data-tab='+dataTab+']').show();
 });
@@ -45,21 +41,15 @@ $(window).scroll(function() {
 
 // Gallery SLide
 
-
-// total of items
-
 let	totalAmount = $('.gallery-slider__item').length;
 $("#total-amount").text(totalAmount);
-
-// counter
 
 var $counter = $('#counter');
 var $slickElement = $('.gallery-slider__list');
 
 $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-    //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
-    var i = (currentSlide ? currentSlide : 0) + 1;
-    $counter.text(i);
+	var i = (currentSlide ? currentSlide : 0) + 1;
+	$counter.text(i);
 });
 
 $('.gallery-slider__list').slick({
@@ -69,16 +59,16 @@ $('.gallery-slider__list').slick({
 	prevArrow: $('#gallery-prev'),
 	nextArrow: $('#gallery-next'),
 	responsive: [
-    {
-      breakpoint: 1025,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    }
-  ]
+		{
+			breakpoint: 1025,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: true
+			}
+		}
+	]
 });
 
 // Testimonials slider
@@ -86,7 +76,7 @@ $('.gallery-slider__list').slick({
 $('.main-slider__list').slick({
 	prevArrow: $('#slider__ctrl-wrapper_left'),
 	nextArrow: $('#slider__ctrl-wrapper_right'),
-    dots: true,
+	dots: true,
 	dotsClass: "my-dots",
 });
 
@@ -98,29 +88,29 @@ $('.partners__list').slick({
 	infinite: true,
 	slidesToShow: 6,
 	slidesToScroll: 3,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: true,
+				dots: true
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+	]
 });
